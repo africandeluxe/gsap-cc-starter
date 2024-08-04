8c2a6e63171ae02c8cd0a818f4078c273c748e95
Africandeluxe <miski.mohamedahmed@gmail.com>
2024-08-04 18:33:27 +0200

    Trigger

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,5 +1,33 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
-  // TODO: Implement the gsap scroll trigger
+ const scrollRef = useRef();
+
+ useGSAP (() => {
+  const boxes = gsap.utils.toArray
+  (scrollRef.current.children);
+
+  boxes.forEach((box) => {
+    gsap.to(box, {
+      x: 150,
+      rotation: 360,
+      borderRadius: '100%',
+      scale: 1.5,
+      scrollTrigger: {
+        trigger: box,
+        start: 'bottom, bottom',
+        end: 'top 20%',
+        scrub: true,
+      },
+      ease: 'power1.inOut'
+    })
+  })
+  }, [])
 
   return (
     <main>
